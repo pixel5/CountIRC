ca721a46c779fb317f9ac1d3de84daefc4568553
Aaron Baxter <aaron@pixel5.co>
2012-09-09 20:17:26 -0500

    v1.0
    BigBen (Rizon) must be in your channel for this to work.

---
+++ irc_count.php
@@ -0,0 +1,28 @@
+<?php
+// IRC Number of Users widget
+// 2012 Aaron Baxter and Chris Lemay 
+// Widget will only work if BigBen is in the channel
+
+// Insert your channel name here, include the #
+$ChannelName = '#novusunion';
+
+
+// DONT TOUCH ANYTHING BELOW
+$file = 'http://www.thecrittac.com/users.txt';
+$contents = file_get_contents($file);
+$pattern = preg_quote($ChannelName, '/');
+$pattern = "/^.*$pattern.*\$/m";
+
+if(preg_match_all($pattern, $contents, $matches)){
+  	$fString = implode("\n", $matches[0]);
+  	$ms = "$fString";
+  	$string = ereg_replace("[^0-9]", "",$ms);
+	echo "Users Online: <a style='color:#009900;'><strong>".$string."</strong></a>";
+}
+else{
+   echo "<a style='color:#cc0000;'>BigBen is down.</a>";
+}
+
+
+
+?>
